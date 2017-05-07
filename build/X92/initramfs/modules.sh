9fddd537083f098223c22a12bd4b403ccf1a71b9
Andrey Zabolotnyi <zapparello@ya.ru>
2017-05-07 17:58:05 +0300

    fixed modules.sh so that it actually works. for some reason android doesn't like dhd.ko being a link to /system/lib/modules/dhd.ko ??? fixed dwc3.ko being loaded from the wrong directory (/boot instead of /lib).

--- build/X92/initramfs/modules.sh
+++ build/X92/initramfs/modules.sh
@@ -1,6 +1,15 @@
 #!/system/bin/sh
 # switch kernel modules depending on currently running kernel
 
+system_is_rw=0
+
+function system_rw() {
+	if [ "$system_is_rw" == "0" ] ; then
+		system_is_rw=1
+		mount -o remount,rw /system
+	fi
+}
+
 if [ ".$1" == ".install" ] ; then
 	# this hack is needed to have modules.sh running
 	# even without the custom kernel & initramfs
@@ -8,42 +17,46 @@ if [ ".$1" == ".install" ] ; then
 	JUNK=/system/bin/ddrtest.sh
 	cmp -s $0 $JUNK && exit 0
 
-	mount -o remount,rw /system
+	system_rw
 	cp $0 $JUNK
 	mount -o remount,ro /system
 
-        # ddrtest was already launched, so continue to our job...
+	exit 0
 fi
 
-# /mnt is tmpfs, so it's suitable for a quick flag file...
-flag=/mnt/.modules_sh
+# /dev is tmpfs, so it's suitable for a quick flag file...
+flag=/dev/.modules_sh
 [ -f $flag ] && exit 0
 touch $flag
 
-system_rw=0
+krel=`uname -r`
+# dhd.ko is loaded by Android at late stage (when WiFi is enabled)
+mods="dhd.ko"
 
-# Check for first boot, move stock drivers to separate directory
-if [ ! -L /system/lib/mali.ko ]; then
-	system_rw=1
-	mount -o remount,rw /system
-	krel=3.14.29
-	mkdir -p /system/lib/modules/$krel
-	mv /system/lib/*.ko /system/lib/modules/$krel
+if [ -d /lib/modules/$krel ] ; then
+	# running a modified kernel
+	for m in $mods ; do
+		l=`readlink /system/lib/$m 2>/dev/null`
+		if [ "$l" != "/lib/modules/$krel/$m" ] ; then
+			system_rw
+			# if module is not a link, it's the stock driver
+			if [ ! -L /system/lib/$m ] ; then
+				mv /system/lib/$m /system/lib/$m.stock
+			fi
+			ln -fs "/lib/modules/$krel/$m" "/system/lib/$m"
+		fi
+	done
+else
+	# running the stock kernel
+	for m in $mods ; do
+		# If module is a link, replace by stock driver
+		if [ -L "/system/lib/$m" ]; then
+			system_rw
+			rm -f "/system/lib/$m"
+			mv /system/lib/$m.stock /system/lib/$m
+		fi
+	done
 fi
 
-krel=`uname -r`
-modir=/lib/modules/$krel
-[ ! -d $modir ] && modir=/system/lib/modules/$krel
-[ ! -d $modir ] && exit 1
-
-for m in $modir/*.ko; do
-	mn=`basename $m`
-	lnk=`readlink /system/lib/$mn 2>/dev/null`
-
-	[ "$m" == "$lnk" ] && continue
-	[ "$system_rw" == "0" ] && system_rw=1 && mount -o remount,rw /system
-	ln -fs $m /system/lib/$mn
-done
-
-[ "$system_rw" == "1" ] && mount -o remount,ro /system
+[ "$system_is_rw" == "1" ] && mount -o remount,ro /system
 rm -f $flag
